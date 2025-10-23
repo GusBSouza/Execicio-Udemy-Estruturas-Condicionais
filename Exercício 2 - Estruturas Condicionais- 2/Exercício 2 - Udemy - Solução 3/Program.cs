6e249fb23e89e88d2c58ec79d07b829c3b523cbe
Gustavo Borges de Souza <160325307+GusBSouza@users.noreply.github.com>
2025-10-23 19:09:17 -0300

    .
    Estruturas Condicionais - 3

---
+++ Exercício 2 - Estruturas Condicionais- 2/Exercício 2 - Udemy - Solução 3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_2___Udemy___Solução_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "";
+            int urgencia;
+
+            Console.WriteLine("Olá, Digite seu nome, por favor.");
+            name = Console.ReadLine();
+            Console.Write("Prazer,");
+            Console.WriteLine(name);
+            Console.WriteLine("Com base na sua urgência, digite de 0 a 10.");
+            urgencia = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("--------------------------------");
+
+            if (urgencia >= 0 & urgencia <= 3)
+            {
+                Console.WriteLine("Seu nível de urgência é, Baixa");
+            }
+            else if (urgencia >= 3 & urgencia <= 6)
+            {
+                Console.WriteLine("Seu nível de urgência é, Média");
+            }
+            else if (urgencia >= 6 & urgencia <= 9)
+            {
+                Console.WriteLine("Seu nível de urgência é, Alta");
+            }
+            else if (urgencia >= 9)
+            {
+                Console.WriteLine("Seu nível de urgência é, Grave");
+            }
+
+            else
+            {
+                Console.WriteLine("Erro na Digitação, tente novamente");
+            }
+            Console.ReadLine();
+        }
+    }
+}
